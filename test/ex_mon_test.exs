97a68c99fb0af19ac50c0e8d909ee14e2d712817
Wagner Santos <wagnersantos@A0194.local>
2021-08-18 19:09:17 -0300

    add new tests do ex_mon

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -25,6 +25,38 @@ defmodule ExMonTest do
       assert messages =~ "status: :started"
       assert messages =~ "turn: :player"
     end
-    
+  end
+
+  describe "make_move/1" do
+    setup do
+      player = Player.build("Peixonauta", :chute, :soco, :cura)
+
+      capture_io(fn ->
+        ExMon.start_game(player)
+      end)
+
+      :ok
+    end
+
+    test "when the move is valid, do the move and the computer makes a move" do
+      messages =
+        capture_io(fn ->
+          ExMon.make_move(:chute)
+        end)
+
+      assert messages =~ "The player attacked the computer"
+      assert messages =~ "It's computer turn"
+      assert messages =~ "status: :continue"
+      assert messages =~ "It's player turn"
+    end
+
+    test "when the move is invalid, returns an error message" do
+      messages =
+      capture_io(fn ->
+        ExMon.make_move(:wrong)
+      end)
+
+    assert messages =~ "Invalid Move: wrong"
+    end
   end
 end
