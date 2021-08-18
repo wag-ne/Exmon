56c51e6b429f75d90956f8d3ed1ecfe452dac8ed
Wagner Santos <wagnersantos@A0194.local>
2021-08-18 18:47:12 -0300

    create game tests

---
+++ test/game_test.exs
@@ -0,0 +1,48 @@
+defmodule ExMon.GameTest do
+    use ExUnit.Case
+
+    alias ExMon.{Game, Player}
+
+    describe "start/2" do
+        test "starts the game state" do
+            player = Player.build("Peixonauta", :chute, :soco, :cura)
+            computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+            assert {:ok, _pid} = Game.start(computer, player)
+        end
+    end
+
+    describe "info/0" do
+        test "returns the current game state" do
+            player = Player.build("Peixonauta", :chute, :soco, :cura)
+            computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+            Game.start(computer, player)
+            
+            expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Robotinik"}, player: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Peixonauta"}, status: :started, turn: :player}
+
+            assert expected_response == Game.info()
+        end
+    end
+
+    describe "update/1" do
+        test "returns the game state updated" do
+            player = Player.build("Peixonauta", :chute, :soco, :cura)
+            computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+            Game.start(computer, player)
+
+            expected_response = %{computer: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Robotinik"}, player: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Peixonauta"}, status: :started, turn: :player}
+
+            assert expected_response == Game.info()
+
+            new_info = %{computer: %Player{life: 80, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Robotinik"}, player: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Peixonauta"}, status: :started, turn: :player}
+
+            Game.update(new_info)
+
+            new_expected_response = %{computer: %Player{life: 80, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Robotinik"}, player: %Player{life: 100, moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute}, name: "Peixonauta"}, status: :continue, turn: :computer}
+
+            assert  new_expected_response == Game.info()
+        end
+    end
+end
